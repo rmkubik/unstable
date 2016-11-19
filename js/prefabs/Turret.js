33096bd680fb7434244f7d54dacc33074dca9cd2
Ryan Kubik <rmkubik@mix.wvu.edu>
2016-11-19 12:37:35 -0800

    turret rotates a bit funkily

--- js/prefabs/Turret.js
+++ js/prefabs/Turret.js
@@ -6,7 +6,8 @@ Unstable.Turret = function (game_state, position, properties) {
 
     game_state.game.physics.arcade.enable(this);
 
-    this.turret = game.add.sprite(position.x, position.y, properties.texture, 1);
+    this.turret = game.add.sprite(position.x + 12, position.y + 12, properties.texture, 1);
+    this.turret.anchor.setTo(0.5);
     this.range = 50;
     this.cooldown = 5;
     this.coolingDown = false;
@@ -16,8 +17,11 @@ Unstable.Turret.prototype = Object.create(Unstable.Prefab.prototype);
 Unstable.Turret.prototype.constructor = Unstable.Turret;
 
 Unstable.Turret.prototype.update = function() {
-  //if player in range && not coolingDown
-  //create hazard with velocity directly at player
+  //rotate turret
+  var rot = this.game_state.game.physics.arcade.angleBetween(this, this.game_state.groups["player"].getTop());
+  this.turret.rotation = rot;
+
+  //shoot Projectile
   if (!this.coolingDown) {
     new Unstable.Projectile(this.game_state, {x:this.position.x + 12, y:this.position.y + 12}, this.turret, {group:"hazards", texture:"enemy_turret", frame:"2", speed:75});
     this.coolingDown = true;
