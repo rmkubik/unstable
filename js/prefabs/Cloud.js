54655caf73a66d51422356a48c4e3a08be473ed6
Ryan Kubik <rmkubik@mix.wvu.edu>
2017-01-30 21:40:31 -0800

    cloup speed :-)

--- js/prefabs/Cloud.js
+++ js/prefabs/Cloud.js
@@ -3,7 +3,13 @@ var Unstable = Unstable || {};
 Unstable.Cloud = function (game_state, position, properties) {
     "use strict";
     Unstable.Prefab.call(this, game_state, position, properties);
+
+    this.speed = properties.speed;
 };
 
 Unstable.Cloud.prototype = Object.create(Unstable.Prefab.prototype);
 Unstable.Cloud.prototype.constructor = Unstable.Cloud;
+
+Unstable.Cloud.update = function() {
+  this.x += this.speed;
+}
