53297f5fdb74d489d59a5e7d92076077a07f8bcc
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-03-14 23:53:21 -0700

    Add easing to coin particles

--- js/prefabs/Coin.js
+++ js/prefabs/Coin.js
@@ -46,7 +46,16 @@ Unstable.Coin = function (game_state, position, properties) {
       burst: true,
       lifetime: 0, //450
       frequency: 10,
-      particleClass: "coin"
+      particleClass: "coin",
+      scale: {
+        minX: 1,
+        maxX: 0,
+        minY: 1,
+        maxY: 0,
+        rate: 5000,
+        ease: Phaser.Easing.Exponential.In,
+        yoyo: false
+      }
     });
 
     this.spawnpoint = {x: this.x, y: this.y};
