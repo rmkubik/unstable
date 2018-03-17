3e0eaac1fe97709bccb634098a24f472a530f483
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-03-16 22:13:29 -0700

    Create a reusable tileset for scenery

---
+++ assets/levels/scenery.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="scenery" tilewidth="24" tileheight="48" tilecount="7" columns="0">
+ <tileoffset x="-12" y="-24"/>
+ <tile id="0">
+  <image width="24" height="24" source="../images/bush3.png"/>
+ </tile>
+ <tile id="1">
+  <image width="24" height="24" source="../images/bush2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="24" height="24" source="../images/bush1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="24" height="48" source="../images/tree3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="24" height="48" source="../images/tree4.png"/>
+ </tile>
+ <tile id="5">
+  <image width="24" height="48" source="../images/tree2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="24" height="48" source="../images/tree1.png"/>
+ </tile>
+</tileset>
