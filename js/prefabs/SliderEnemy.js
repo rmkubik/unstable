ac5666329ecc5545431e97d2d60f69383ed4bb2f
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-03-31 22:25:17 -0700

    Conditionally check for player offset from Slider

--- js/prefabs/SliderEnemy.js
+++ js/prefabs/SliderEnemy.js
@@ -7,16 +7,19 @@ Unstable.SliderEnemy = function (game_state, position, properties) {
     this.anchor.setTo(0.5, 0.5);
 
     this.cooldown = 3;
+    this.speed = 90;
     this.coolingDown = false;
-    this.active = false;
+    this.axis = 'x';
+    this.firingOffsetTolerance = 50;
+
 };
 
 Unstable.SliderEnemy.prototype = Object.create(Unstable.Prefab.prototype);
 Unstable.SliderEnemy.prototype.constructor = Unstable.SliderEnemy;
 
 Unstable.SliderEnemy.prototype.update = function() {
-  //shoot Projectile
-  if (!this.coolingDown) {
+  var diff = this.game_state.player.x - this.x;
+  if (Math.abs(diff) <= this.firingOffsetTolerance && !this.coolingDown) {
       this.game_state.game.sound.play("sfx_turretShoot");
       new Unstable.Projectile(this.game_state, {x:this.position.x, y:this.position.y}, {x:75, y:0} ,{
         group:"hazards", texture:"enemy_sheet", frame:"4", speed:75, rgroup:"objects", pgroup:"hazards"
@@ -24,6 +27,9 @@ Unstable.SliderEnemy.prototype.update = function() {
       this.coolingDown = true;
       game.time.events.add(Phaser.Timer.SECOND * this.cooldown, this.resetCooldown, this);
   }
+  // switch x/y axis
+  // if player more than MAGICNUMBER units away in axis, move toward them
+  // else stop and shoot
 }
 
 Unstable.SliderEnemy.prototype.resetCooldown = function() {
