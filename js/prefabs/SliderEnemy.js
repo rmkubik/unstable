427b7d213f926d7745aea1c3266d6ace2c8819ca
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-04-01 20:09:29 -0700

    Allow Slider to slide on x or y axis

--- js/prefabs/SliderEnemy.js
+++ js/prefabs/SliderEnemy.js
@@ -5,34 +5,73 @@ Unstable.SliderEnemy = function (game_state, position, properties) {
     Unstable.Prefab.call(this, game_state, position, properties);
 
     game_state.game.physics.arcade.enable(this);
-    this.anchor.setTo(0.5, 1);
 
     this.cooldown = 3;
     this.speed = 90;
     this.coolingDown = false;
-    this.axis = 'x';
+    this.shotSpeed = 75;
+
+    this.axis = properties.axis;
     this.firingOffsetTolerance = 10;
     this.prevDirection;
 
+    switch (this.axis) {
+        case 'x':
+            this.angle = 90;
+            this.anchor.setTo(0, 0.5);
+            break;
+        case 'y':
+            this.angle = 0;
+            this.anchor.setTo(0.5, 1);
+            break;
+        default:
+            console.error('Invalid Slider Axis: ' + this.axis);
+            break;
+    }
 };
 
 Unstable.SliderEnemy.prototype = Object.create(Unstable.Prefab.prototype);
 Unstable.SliderEnemy.prototype.constructor = Unstable.SliderEnemy;
 
 Unstable.SliderEnemy.prototype.update = function() {
-  var diff = this.game_state.player.y - this.y;
-  if (Math.abs(diff) <= this.firingOffsetTolerance && !this.coolingDown) {
-      this.game_state.game.sound.play("sfx_turretShoot");
-      new Unstable.Projectile(
+    var projVelocity = {
+        x: 0,
+        y: 0
+    }
+    switch (this.axis) {
+        case 'x':
+            if (this.y < this.game_state.player.y) {
+                this.scale.x = 1;
+                projVelocity.y = this.shotSpeed;
+            } else {
+                this.scale.x = -1;
+                projVelocity.y = -this.shotSpeed;
+            }
+            break;
+        case 'y':
+            if (this.x < this.game_state.player.x) {
+                this.scale.x = 1;
+                projVelocity.x = this.shotSpeed;
+            } else {
+                this.scale.x = -1;
+                projVelocity.x = -this.shotSpeed;
+            }
+            break;
+        default:
+            console.error('Invalid Slider Axis: ' + this.axis);
+            break;
+    }
+
+    var diff = this.game_state.player[this.axis] - this[this.axis];
+    if (Math.abs(diff) <= this.firingOffsetTolerance && !this.coolingDown) {
+        this.game_state.game.sound.play("sfx_turretShoot");
+        new Unstable.Projectile(
           this.game_state,
           {
               x: this.position.x,
               y: this.position.y - this.body.height/2
           },
-          {
-              x: 75,
-              y: 0
-          },
+          projVelocity,
           {
               group: "hazards",
               texture: "enemy_sheet",
@@ -41,23 +80,22 @@ Unstable.SliderEnemy.prototype.update = function() {
               rgroup: "objects",
               pgroup: "hazards"
           }
-      );
-      this.coolingDown = true;
-      game.time.events.add(Phaser.Timer.SECOND * this.cooldown, this.resetCooldown, this);
-  } else {
-      var x = 0;
-      var y = 0;
-      if (this.prevDirection === Math.sign(diff)) {
-          y = Math.sign(diff) * this.speed;
-      } else if (this.prevDirection !== undefined) {
-          this.y = this.game_state.player.y;
+        );
+        this.coolingDown = true;
+        game.time.events.add(Phaser.Timer.SECOND * this.cooldown, this.resetCooldown, this);
+    } else {
+      var pos = {
+          x: 0,
+          y: 0
       }
-      this.body.velocity.setTo(x, y);
+      if (this.prevDirection === Math.sign(diff)) {
+          pos[this.axis] = Math.sign(diff) * this.speed;
+      } else if (this.prevDirection !== undefined) {
+          this[this.axis] = this.game_state.player[this.axis];
+      }
+      this.body.velocity.setTo(pos.x, pos.y);
       this.prevDirection = Math.sign(diff);
-  }
-  // switch x/y axis
-  // if player more than MAGICNUMBER units away in axis, move toward them
-  // else stop and shoot
+    }
 }
 
 Unstable.SliderEnemy.prototype.resetCooldown = function() {
