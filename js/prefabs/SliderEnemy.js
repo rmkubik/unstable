8227964e207a1823adbf7c54af566784bb6c8efa
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-04-01 18:06:38 -0700

    Have slider snap to player position

--- js/prefabs/SliderEnemy.js
+++ js/prefabs/SliderEnemy.js
@@ -4,13 +4,15 @@ Unstable.SliderEnemy = function (game_state, position, properties) {
     "use strict";
     Unstable.Prefab.call(this, game_state, position, properties);
 
-    this.anchor.setTo(0.5, 0.5);
+    game_state.game.physics.arcade.enable(this);
+    this.anchor.setTo(0.5, 1);
 
     this.cooldown = 3;
     this.speed = 90;
     this.coolingDown = false;
     this.axis = 'x';
-    this.firingOffsetTolerance = 50;
+    this.firingOffsetTolerance = 10;
+    this.prevDirection = 0;
 
 };
 
@@ -18,14 +20,40 @@ Unstable.SliderEnemy.prototype = Object.create(Unstable.Prefab.prototype);
 Unstable.SliderEnemy.prototype.constructor = Unstable.SliderEnemy;
 
 Unstable.SliderEnemy.prototype.update = function() {
-  var diff = this.game_state.player.x - this.x;
+  var diff = this.game_state.player.y - this.y;
   if (Math.abs(diff) <= this.firingOffsetTolerance && !this.coolingDown) {
       this.game_state.game.sound.play("sfx_turretShoot");
-      new Unstable.Projectile(this.game_state, {x:this.position.x, y:this.position.y}, {x:75, y:0} ,{
-        group:"hazards", texture:"enemy_sheet", frame:"4", speed:75, rgroup:"objects", pgroup:"hazards"
-      });
+      new Unstable.Projectile(
+          this.game_state,
+          {
+              x: this.position.x,
+              y: this.position.y - this.body.height/2
+          },
+          {
+              x: 75,
+              y: 0
+          },
+          {
+              group: "hazards",
+              texture: "enemy_sheet",
+              frame: "4",
+              speed: 75,
+              rgroup: "objects",
+              pgroup: "hazards"
+          }
+      );
       this.coolingDown = true;
       game.time.events.add(Phaser.Timer.SECOND * this.cooldown, this.resetCooldown, this);
+  } else {
+      var x = 0;
+      var y = 0;
+      if (this.prevDirection === Math.sign(diff)) {
+          y = Math.sign(diff) * this.speed;
+      } else {
+          this.y = this.game_state.player.y;
+      }
+      this.body.velocity.setTo(x, y);
+      this.prevDirection = Math.sign(diff);
   }
   // switch x/y axis
   // if player more than MAGICNUMBER units away in axis, move toward them
