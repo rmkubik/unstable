ee57370c3c083070ad2a5bb5cdae8eb624b591f2
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-04-05 22:14:57 -0700

    Fix h2l5 by embedding tilesets

--- assets/levels/tilesets/enemy_bomb.tsx
+++ assets/levels/tilesets/enemy_bomb.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="enemy_bomb" tilewidth="24" tileheight="24" spacing="1" tilecount="4" columns="4">
  <tileoffset x="-12" y="-24"/>
- <image source="../images/enemy_bomb.png" width="99" height="24"/>
+ <image source="../../images/enemy_bomb.png" width="99" height="24"/>
 </tileset>
