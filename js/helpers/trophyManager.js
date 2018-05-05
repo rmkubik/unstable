3acdeba06ad6962b6f7017b2a86e812d9e9f63ff
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-05-05 11:55:57 -0700

    Create Trophy Manager helper

---
+++ js/helpers/trophyManager.js
@@ -0,0 +1,13 @@
+function trophyManager(state) {
+    var initiallyCompleted = state.completion === 1;
+    var times = state.times;
+
+    return {
+        render: function(newState) {
+
+        },
+        calcTrophies(state) {
+            
+        }
+    }
+}
