7f1e44124ac8166f48798e54392d729d9593d8b6
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-05-08 23:16:59 -0700

    Parse level times snippet

--- snippets.js
+++ snippets.js
@@ -1,2 +1,17 @@
 const scores = require('./ryan_scores_1.json');
-Object.entries(scores.levels).map(([key, val]) => val.times.map(r => [key,r.time])).reduce((acc, val) => acc.concat(val), []);
+const times = Object.entries(scores.levels)
+    .filter(
+        ([key, val]) => (
+            val.times.length === 0
+        )
+    )
+    .map(
+        ([key, val]) => (
+            // val.times.map(r => [key,r.time])
+            [key, val.times]
+        )
+    )
+    .reduce(
+        (acc, val) => acc.concat(val), []
+    );
+console.log(times);
