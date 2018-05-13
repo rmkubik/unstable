a5c09d915679f61ed1ac0796c2e6d1fdfa8905a8
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-05-13 13:25:30 -0700

    Update volume manager to add and set volume

--- js/helpers/VolumeManager.js
+++ js/helpers/VolumeManager.js
@@ -1,4 +1,25 @@
-function VolumeManager(gameState, volumes) {
-    
+function VolumeManager(gameState) {
+    this.sfx = {};
+    this.tracks = {};
 
+    return {
+        addSfx: function(key, soundFileKey) {
+            this.sfx[key] = this.game.add.sound(soundFileKey);
+        },
+        addTrack: function(key, trackFileKey) {
+            this.tracks[key] = this.game.add.sound(trackFileKey);
+        },
+        setSfxVolume: function(vol) {
+            var sfxKeys = Object.keys(this.sfx);
+            sfxKeys.forEach(function(sound) {
+                this.sfx[sound].volume = vol;
+            });
+        },
+        setMusicVolume: function(vol) {
+            var trackKeys = Object.keys(this.tracks);
+            trackKeys.forEach(function(track) {
+                this.tracks[track].volume = vol;
+            });
+        }
+    }
 }
