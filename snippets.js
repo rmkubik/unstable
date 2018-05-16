db8f170eb89a39009b63213f19badcf90c44283b
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-05-15 23:27:52 -0700

    Update snippets

--- snippets.js
+++ snippets.js
@@ -1,17 +1,19 @@
-const scores = require('./ryan_scores_1.json');
+const scores = require('./ryan_scores_3.json');
 const times = Object.entries(scores.levels)
     .filter(
         ([key, val]) => (
-            val.times.length === 0
+            val.times.length > 0
         )
     )
     .map(
         ([key, val]) => (
-            // val.times.map(r => [key,r.time])
-            [key, val.times]
+            // val.times[0].time
+            [key, ...val.times.map(r => r.time)]
+            // [key, val.times]
         )
     )
-    .reduce(
-        (acc, val) => acc.concat(val), []
-    );
+    // .reduce(
+    //     (acc, val) => acc.concat(val),
+    //     []
+    // );
 console.log(times);
